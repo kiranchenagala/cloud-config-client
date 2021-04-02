f014b4961147cc4a48f07b6f9d4b48b4e5d86356
kk <kiranchenagala@gmail.com>
2021-04-02 16:10:02 +0530

    initial commit

---
+++ src/test/java/com/kk/configclient/cloudconfigclient/CloudConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.configclient.cloudconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigClientApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
